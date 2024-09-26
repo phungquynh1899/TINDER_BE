b6479975876041bdd19fa483cdd93632f4bae82f
Phung Quynh Nguyen Truong <phungquynh1899@gmail.com>
2024-09-26 13:50:53 +0700

    Buoi2-exploring the http methods

--- src/app.js
+++ src/app.js
@@ -2,11 +2,23 @@
 const express = require('express');
 const app = express();
 //thêm request handler, bắt buộc theo thứ tự
-app.use('/hi', (request, response)=>{
-    response.send('hi from server')
+// app.use('/hi', (request, response)=>{
+//     response.send('hi from server')
+// })
+// app.use("/", (request, response)=>{
+//     response.send('/ from server')
+// })
+
+app.get("/get", (req, res)=>{
+    res.send('get from get method')
+});
+
+app.post('/post', (req, res)=>{
+    res.send('post from server');
 })
-app.use("/", (request, response)=>{
-    response.send('/ from server')
+
+app.delete('/delete',(req, res)=>{
+    res.send('delete from server');
 })
 
 app.listen(8889);
